ba51995de48a4a36bc71e57cd1196ec602bbfb40
DeveloperKairo <contatodevkairo@gmail.com>
2025-11-01 13:59:40 -0300

    feat: new requistions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
+
 function convertPokemonToLi(pokemon) {
   return `
       <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
           <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
           </ol>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+          <img src="${pokemon.sprite}" alt="${pokemon.name}">
         </div>
       </li>
   `;
@@ -17,6 +17,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('');
 })
 
